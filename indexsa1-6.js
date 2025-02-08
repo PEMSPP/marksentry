5f88e46ced0221721b3c52f8b64a85a18a9ce801
PEMSPP <pennaschool.talaricheruvu@gmail.com>
2025-02-08 20:39:38 +0530

    Update indexsa1-6.js

--- indexsa1-6.js
+++ indexsa1-6.js
@@ -262,12 +262,12 @@ function StudentMarksEntry() {
     
         const headers2 = [
             "", "", "", "",
-            "SA1-20M", "SA1-0M", "Total", "Grade", "SGPA",
-            "SA1-20M", "SA1-70M", "Total", "Grade", "SGPA",
-            "SA1-20M", "SA1-70M", "Total", "Grade", "SGPA",
-            "SA1-20M", "SA1-70M", "Total", "Grade", "SGPA",
-            "SA1-20M", "SA1-70M", "Total", "Grade", "SGPA",
-            "SA1-20M", "SA1-70M", "Total", "Grade", "SGPA",
+            "20M", "SA1-80M", "Total", "Grade", "SGPA",
+            "20M", "SA1-80M", "Total", "Grade", "SGPA",
+            "20M", "SA1-80M", "Total", "Grade", "SGPA",
+            "0M", "SA1-80M", "Total", "Grade", "SGPA",
+            "20M", "SA1-80M", "Total", "Grade", "SGPA",
+            "20M", "SA1-80M", "Total", "Grade", "SGPA",
             "", "", "", ""
         ];
     
