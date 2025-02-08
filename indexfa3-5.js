3a4735d708433907db59263c02b383ffb0fd8c26
PEMSPP <pennaschool.talaricheruvu@gmail.com>
2025-02-08 20:37:29 +0530

    Update indexfa3-5.js

--- indexfa3-5.js
+++ indexfa3-5.js
@@ -190,11 +190,11 @@ function StudentMarksEntry() {
     
         const headers2 = [
             "", "", "", "",
-            "FA1-20M", "Speaking", "Basic Knowledge", "Writing", "Corrections", "Behaviour", "Activity", "SubTotal", "Grade", "SGPA",
-            "FA1-20M", "Speaking", "Basic Knowledge", "Writing", "Corrections", "Behaviour", "Activity", "SubTotal", "Grade", "SGPA",
-            "FA1-20M", "Speaking", "Basic Knowledge", "Writing", "Corrections", "Behaviour", "Activity", "SubTotal", "Grade", "SGPA",
-            "FA1-20M", "Speaking", "Basic Knowledge", "Writing", "Corrections", "Behaviour", "Activity", "SubTotal", "Grade", "SGPA",
-            "FA1-20M", "Speaking", "Basic Knowledge", "Writing", "Corrections", "Behaviour", "Activity", "SubTotal", "Grade", "SGPA",
+            "FA3-20M", "Speaking", "Basic Knowledge", "Writing", "Corrections", "Behaviour", "Activity", "SubTotal", "Grade", "SGPA",
+            "FA3-20M", "Speaking", "Basic Knowledge", "Writing", "Corrections", "Behaviour", "Activity", "SubTotal", "Grade", "SGPA",
+            "FA3-20M", "Speaking", "Basic Knowledge", "Writing", "Corrections", "Behaviour", "Activity", "SubTotal", "Grade", "SGPA",
+            "FA3-20M", "Speaking", "Basic Knowledge", "Writing", "Corrections", "Behaviour", "Activity", "SubTotal", "Grade", "SGPA",
+            "FA3-20M", "Speaking", "Basic Knowledge", "Writing", "Corrections", "Behaviour", "Activity", "SubTotal", "Grade", "SGPA",
             "", "", "", ""
         ];
     
@@ -312,7 +312,7 @@ function StudentMarksEntry() {
                             </tr>
                             <tr>
                                 {["Telugu", "Hindi", "English", "Mathematics", "Social"].flatMap(subject =>
-                                    ["FA1-20M", "Speaking", "Basic Knowledge", "Writing", "Corrections", "Behaviour", "Activity", "SubTotal", "Grade", "SGPA"]
+                                    ["FA3-20M", "Speaking", "Basic Knowledge", "Writing", "Corrections", "Behaviour", "Activity", "SubTotal", "Grade", "SGPA"]
                                         .map((sub, i) => <th key={`${subject}-${sub}`}>{sub}</th>)
                                 )}
                             </tr>
