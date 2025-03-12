65aaaaf4fcb5de20372fd9a52446ad8f6e226009
PEMSPP <pennaschool.talaricheruvu@gmail.com>
2025-03-12 14:41:53 +0530

    Update indexfa4-7.js

--- indexfa4-7.js
+++ indexfa4-7.js
@@ -206,12 +206,12 @@ function StudentMarksEntry() {
 
         const headers2 = [
             "", "", "", "",
-            "FA1-20M", "Children's Participation", "Written Work", "Speaking", "Behaviour", "SubTotal", "Grade", "SGPA",
-            "FA1-20M", "Children's Participation", "Written Work", "Speaking", "Behaviour", "SubTotal", "Grade", "SGPA",
-            "FA1-20M", "Children's Participation", "Written Work", "Speaking", "Behaviour", "SubTotal", "Grade", "SGPA",
-            "FA1-20M", "Children's Participation", "Written Work", "Speaking", "Behaviour", "SubTotal", "Grade", "SGPA",
-            "FA1-20M", "Children's Participation", "Written Work", "Speaking", "Behaviour", "SubTotal", "Grade", "SGPA",
-            "FA1-20M", "Children's Participation", "Written Work", "Speaking", "Behaviour", "SubTotal", "Grade", "SGPA",
+            "FA4-20M", "Children's Participation", "Written Work", "Speaking", "Behaviour", "SubTotal", "Grade", "SGPA",
+            "FA4-20M", "Children's Participation", "Written Work", "Speaking", "Behaviour", "SubTotal", "Grade", "SGPA",
+            "FA4-20M", "Children's Participation", "Written Work", "Speaking", "Behaviour", "SubTotal", "Grade", "SGPA",
+            "FA4-20M", "Children's Participation", "Written Work", "Speaking", "Behaviour", "SubTotal", "Grade", "SGPA",
+            "FA4-20M", "Children's Participation", "Written Work", "Speaking", "Behaviour", "SubTotal", "Grade", "SGPA",
+            "FA4-20M", "Children's Participation", "Written Work", "Speaking", "Behaviour", "SubTotal", "Grade", "SGPA",
             "", "", "", ""
         ];
 
@@ -311,7 +311,7 @@ function StudentMarksEntry() {
                             </tr>
                             <tr>
                                 {['Telugu', 'Hindi', 'English', 'Mathematics', 'Science', 'Social'].flatMap(subject =>
-                                    ['FA1-20M', "Children's Participation", 'Written Work', 'Speaking', 'Behaviour', 'SubTotal', 'Grade', 'SGPA']
+                                    ['FA4-20M', "Children's Participation", 'Written Work', 'Speaking', 'Behaviour', 'SubTotal', 'Grade', 'SGPA']
                                         .map(sub => <th key={`${subject}-${sub}`}>{sub}</th>)
                                 )}
                             </tr>
