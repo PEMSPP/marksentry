2595edc92857d21deef4930e4dcc528699ceadc5
PEMSPP <pennaschool.talaricheruvu@gmail.com>
2025-04-17 10:52:50 +0530

    Update indexsa2-6.js

--- indexsa2-6.js
+++ indexsa2-6.js
@@ -54,7 +54,7 @@ function StudentMarksEntry() {
 
     useEffect(() => {
         const fetchSchoolDataFromFirebase = async (school) => {
-            const response = await axios.get(`https://marksentry2024-default-rtdb.firebaseio.com/2024/SA-1/schools/${school}/Class-6.json`);
+            const response = await axios.get(`https://marksentry2024-default-rtdb.firebaseio.com/2024/SA-2/schools/${school}/Class-6.json`);
             const data = response.data || [];
             return Object.keys(data).map((key, index) => ({
                 sno: index + 1,
@@ -205,7 +205,7 @@ function StudentMarksEntry() {
         alert('Data is saving to the database...');
 
         axios
-            .put(`https://marksentry2024-default-rtdb.firebaseio.com/2024/SA-1/schools/${selectedSchool}/Class-6.json`, students)
+            .put(`https://marksentry2024-default-rtdb.firebaseio.com/2024/SA-2/schools/${selectedSchool}/Class-6.json`, students)
             .then(() => {
                 // Notify the user that data is saved successfully
                 alert('Data saved successfully!');
