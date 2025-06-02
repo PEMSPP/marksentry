358a3a0bb1e22773f8f6121ee3654492d23ff0ab
PEMSPP <pennaschool.talaricheruvu@gmail.com>
2025-06-02 12:01:51 +0530

    Update indexsa1-2.js

--- indexsa1-2.js
+++ indexsa1-2.js
@@ -392,6 +392,8 @@ function StudentMarksEntry() {
                                                         onChange={e => handleInputChange(index, subject, subIndex, e.target.value)}
                                                         onKeyDown={e => handleKeyDown(e, index, subject, subIndex)} 
                                                         style={{ width: '30px' }} 
+disabled={selectedSchool === 'Talaricheruvu' || 'Boyareddypalli' || 'Mantapampalli' || 'Ganesh Pahad' || 'Tandur' || 'ALL' }
+                                                    
                                                     />
                                                 </td>
                                             ))}
